5283a9f2fce3dbc0763e4eb65086a5c7f76887b7
nimrodyahal <nimrodyahal@gmail.com>
2018-03-13 16:42:13 +0200

    More work on preprocessing: finished word segmentation. Working on contour detection in individual words - some success; more work to be done.

--- code/cleaned/test_contours.py
+++ code/cleaned/test_contours.py
@@ -25,8 +25,10 @@ def rotate_to_text_tilt(img, angle):
 def get_line_coords(img, threshold):
     hist = cv2.reduce(img, 1, cv2.REDUCE_AVG).reshape(-1)
     h, w = img.shape[:2]
-    uppers = [y for y in range(h - 1) if hist[y] <= threshold < hist[y + 1]]
-    lowers = [y for y in range(h - 1) if hist[y] > threshold >= hist[y + 1]]
+    uppers = [y for y in range(h - 1) if
+              hist[y] <= threshold < hist[y + 1]]
+    lowers = [y for y in range(h - 1) if
+              hist[y] > threshold >= hist[y + 1]]
     return uppers, lowers
 
 
@@ -34,30 +36,29 @@ def get_word_coords(img, threshold):
     slices = cv2.reduce(img, 0, cv2.REDUCE_AVG).reshape(-1)
     count = 0
     is_line = False
-    y = 0
+    # y = 0
     line_coords = []
-    for i, _ in enumerate(slices):
+    for i, slc in enumerate(slices):
         if is_line:
-            if not slices[i]:
+            if not slc:
                 is_line = False
                 count = 1
-                y = i
+                # y = i
         else:
-            if slices[i]:
+            if slc:
                 is_line = True
                 if count >= threshold:
-                    line_coords.append(int(y / count))
+                    # line_coords.append(int(y / count))
+                    line_coords.append(i - count / 2)
             else:
-                y += i
+                # y += i
                 count += 1
     return line_coords
-    # img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-    # return get_line_coords(img, threshold)
-    # hist = cv2.reduce(img, 0, cv2.REDUCE_AVG).reshape(-1)
-    # h, w = img.shape[:2]
-    # lefts = [x for x in range(w - 1) if hist[x] <= threshold < hist[x + 1]]
-    # rights = [x for x in range(w - 1) if hist[x] > threshold >= hist[x + 1]]
-    # return lefts, rights
+
+
+def get_space_ratio(line_height):
+    # return line_height / 2.67
+    return line_height / 5
 
 
 def main():
@@ -67,31 +68,43 @@ def main():
     thresh = cv2.bitwise_not(thresh)
 
     angle = find_text_tilt(thresh)
-    rotated_thresh = rotate_to_text_tilt(thresh, angle)
-    rotated_gray = rotate_to_text_tilt(imgray, angle)
+    rot_th = rotate_to_text_tilt(thresh, angle)
 
-    uppers, lowers = get_line_coords(rotated_thresh, 1)
+    rotated_gray = rotate_to_text_tilt(imgray, angle)
+    rotated_gray = cv2.cvtColor(rotated_gray, cv2.COLOR_GRAY2BGR)
+
+    uppers, lowers = get_line_coords(rot_th, 1)
     # rotated_thresh = cv2.cvtColor(rotated_thresh, cv2.COLOR_GRAY2BGR)
 
     for upper, lower in zip(uppers, lowers):
-        words = get_word_coords(rotated_thresh[upper:lower], 7.4)
-        for x in words:
-            cv2.line(rotated_thresh, (x, upper), (x, lower), (255, 0, 0), 1)
-        # lefts, rights = get_word_coords(rotated_thresh[upper:lower], 1)
-        # for x in lefts:
-        #     cv2.line(rotated_thresh, (x, upper), (x, lower), (255, 0, 0), 1)
-        # for x in rights:
-        #     cv2.line(rotated_thresh, (x, upper), (x, lower), (255, 0, 0), 1)
-    cv2.imshow('bla', rotated_thresh)
+        space_len = get_space_ratio(lower - upper)
+        words = get_word_coords(rot_th[upper:lower], space_len)
+        # for x in words:
+        #     cv2.line(rot_th, (x, upper), (x, lower), (255, 0, 0), 1)
 
-    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,
-                                              cv2.CHAIN_APPROX_SIMPLE)
+        words.append(rot_th.shape[:2][1])
 
-    contours = np.array(contours)
-    contours = contours[np.where(hierarchy[0, :, 3] == 0)]
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        for left, right in zip(words[:-1], words[1:]):
+            # cv2.line(rotated_thresh, (left, upper), (right, upper),
+            #          (255, 0, 0), 1)
+            _, contours, hierarchy = cv2.findContours(rot_th[upper:lower,
+                                                      left:right],
+                                                      cv2.RETR_TREE,
+                                                      cv2.CHAIN_APPROX_SIMPLE)
+            contours = np.array(contours)
+            contours = contours[np.where(hierarchy[0, :, 3] == -1)]
+            for cnt in contours:
+                x, y, w, h = cv2.boundingRect(cnt)
+                if h < (lower - upper) / 3:
+                    continue
+                # print x, y, w, h
+                # print hierarchy
+                cv2.rectangle(rotated_gray[upper:lower, left:right], (x, y),
+                              (x + w, y + h), (255, 0, 0), 1)
+    cv2.imshow('bla', rotated_gray)
+    cv2.imshow('bla2', rot_th)
+
+
     # cv2.imshow('im', im)
 
     cv2.waitKey(0)
