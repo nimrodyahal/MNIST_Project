d0aa99b8e1364d17c059008416212929c878731a
nimrodyahal <nimrodyahal@gmail.com>
2018-03-16 07:17:08 +0200

    More work on preprocessing - contours, SON!

--- code/cleaned/test_contours.py
+++ code/cleaned/test_contours.py
@@ -56,13 +56,8 @@ def get_word_coords(img, threshold):
     return line_coords
 
 
-def get_space_ratio(line_height):
-    # return line_height / 2.67
-    return line_height / 5
-
-
-def main():
-    im = cv2.imread('..\\testing text.png', 1)
+def load_image(path):
+    im = cv2.imread(path, 1)
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(imgray, 215, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.bitwise_not(thresh)
@@ -70,46 +65,82 @@ def main():
     angle = find_text_tilt(thresh)
     rot_th = rotate_to_text_tilt(thresh, angle)
 
-    rotated_gray = rotate_to_text_tilt(imgray, angle)
-    rotated_gray = cv2.cvtColor(rotated_gray, cv2.COLOR_GRAY2BGR)
+    rot_gray = rotate_to_text_tilt(imgray, angle)
+    rot_gray = cv2.cvtColor(rot_gray, cv2.COLOR_GRAY2BGR)
+    return rot_th, rot_gray
+
+
+def detect_contours(img, thresh_img, line_height):
+    _, contours, hierarchy = cv2.findContours(255 - thresh_img,
+                                              cv2.RETR_EXTERNAL,
+                                              cv2.CHAIN_APPROX_SIMPLE)
+    # contours = np.array(contours)
+    # contours = contours[np.where(hierarchy[0, :, 3] == -1)]
+    for i, cnt in enumerate(contours):
+        # all_contours = contours[np.where(hierarchy[0, :, 3] == i)]
+        # np.append([cnt], all_contours)
+        x, y, w, h = cv2.boundingRect(cnt)
+        x -= 1
+        y -= 1
+        w += 2
+        h += 2
+        if h < line_height / 5:
+            continue
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+    # thresh = np.zeros_like(thresh_img)
+    # cv2.drawContours(thresh, contours, -1, (255, 255, 0), cv2.FILLED)
+    # cv2.imshow('thresh{}'.format(id(cnt)), thresh)
+    cv2.imshow('thresh', img)
+
+
+def tittle_segmentation(contours):
+    """
+    Tittle is the dot above 'i' and 'j'. It causes problems when segmenting
+    into individual characters, so this functions fixes that by comparing the
+    coordinates of different contour bounding boxes to find ones that are right
+    above one another, then combining them.
+    """
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        x2 = x + w
+        y2 = y + h
+    pass
+
+
+def get_space_ratio(line_height):
+    return line_height / 5
+
+
+def main():
+    rot_th, rot_gray = load_image('..\\testing images\\testing text.png')
 
     uppers, lowers = get_line_coords(rot_th, 1)
-    # rotated_thresh = cv2.cvtColor(rotated_thresh, cv2.COLOR_GRAY2BGR)
 
     for upper, lower in zip(uppers, lowers):
         space_len = get_space_ratio(lower - upper)
         words = get_word_coords(rot_th[upper:lower], space_len)
-        # for x in words:
-        #     cv2.line(rot_th, (x, upper), (x, lower), (255, 0, 0), 1)
-
         words.append(rot_th.shape[:2][1])
 
         for left, right in zip(words[:-1], words[1:]):
-            # cv2.line(rotated_thresh, (left, upper), (right, upper),
-            #          (255, 0, 0), 1)
-            _, contours, hierarchy = cv2.findContours(rot_th[upper:lower,
-                                                      left:right],
-                                                      cv2.RETR_TREE,
-                                                      cv2.CHAIN_APPROX_SIMPLE)
-            contours = np.array(contours)
-            contours = contours[np.where(hierarchy[0, :, 3] == -1)]
-            for cnt in contours:
-                x, y, w, h = cv2.boundingRect(cnt)
-                if h < (lower - upper) / 3:
-                    continue
-                # print x, y, w, h
-                # print hierarchy
-                cv2.rectangle(rotated_gray[upper:lower, left:right], (x, y),
-                              (x + w, y + h), (255, 0, 0), 1)
-    cv2.imshow('bla', rotated_gray)
+            detect_contours(rot_gray, rot_th[upper:lower, left:right], lower - upper)
+    cv2.imshow('bla', rot_gray)
     cv2.imshow('bla2', rot_th)
-
-
-    # cv2.imshow('im', im)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    im = cv2.imread('..\\testing images\\testing text.png', 1)
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    detect_contours(im, thresh, 100)
+    # _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,
+    #                                           cv2.CHAIN_APPROX_SIMPLE)
+    # contours = np.array(contours)
+    # contours = contours[np.where(hierarchy[0, :, 3] == 0)]
+    # cv2.drawContours(im, contours, -1, (0, 255, 0), 1)
+    cv2.imshow('bla', im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
